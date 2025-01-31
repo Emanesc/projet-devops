50d7b3287324523ee300362221b9a4164a8cdf99
Emanesc <sidicheikh.emane@gmail.com>
2025-01-31 21:59:32 +0000

    app run changement

--- app.py
+++ app.py
@@ -215,6 +215,4 @@ def logout():
     return redirect(url_for('homelogin'))
 
 if __name__ == '__main__':
-   with app.app_context():
-        db.create_all()
-        app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
